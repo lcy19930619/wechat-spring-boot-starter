dddc7f8b527c272f6bc4060d2ab7230ef503348b
冷春阳 <lengchunyang@qianlima.com>
2021-01-20 16:20:18 +0800

    fix

--- src/main/java/net/jlxxw/component/weixin/response/WeiXinMessageResponse.java
+++ src/main/java/net/jlxxw/component/weixin/response/WeiXinMessageResponse.java
@@ -5,6 +5,70 @@ package net.jlxxw.component.weixin.response;
  * @author chunyang.leng
  * @date 2021/1/20 11:51 上午
  */
-public abstract class WeiXinMessageResponse {
+public class WeiXinMessageResponse {
+    /**
+     * 接收方帐号（收到的OpenID）
+     */
+    private String ToUserName;
 
+    /**
+     * 开发者微信号
+     */
+    private String FromUserName;
+
+    /**
+     * 消息创建时间 （整型）
+     */
+    private Long CreateTime;
+
+    /**
+     * 消息类型，
+     * 文本为text
+     */
+    private String MsgType;
+
+    /**
+     * 通过素材管理中的接口上传多媒体文件，得到的id。
+     */
+    private String MediaId;
+
+    public String getToUserName() {
+        return ToUserName;
+    }
+
+    public void setToUserName(String toUserName) {
+        ToUserName = toUserName;
+    }
+
+    public String getFromUserName() {
+        return FromUserName;
+    }
+
+    public void setFromUserName(String fromUserName) {
+        FromUserName = fromUserName;
+    }
+
+    public Long getCreateTime() {
+        return CreateTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        CreateTime = createTime;
+    }
+
+    public String getMsgType() {
+        return MsgType;
+    }
+
+    public void setMsgType(String msgType) {
+        MsgType = msgType;
+    }
+
+    public String getMediaId() {
+        return MediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        MediaId = mediaId;
+    }
 }
--- src/main/java/net/jlxxw/component/weixin/util/JsonToXmlUtils.java
+++ src/main/java/net/jlxxw/component/weixin/util/JsonToXmlUtils.java
@@ -1,7 +1,10 @@
 package net.jlxxw.component.weixin.util;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import org.xml.sax.SAXException;
+
+import java.util.Objects;
 
 /**
  * @author chunyang.leng
@@ -35,13 +38,37 @@ public class JsonToXmlUtils {
         json.forEach((key,v)->{
             Object child = json.get(key);
             if (child instanceof JSONObject) {
-                stringBuffer.append("<").append(key).append(">").append(
-                        jsonToDocument(json.getJSONObject(key),stringBuffer)
-                ).append("</").append(key).append(">");
+                stringBuffer
+                        .append("<").append(key).append(">")
+                        .append(jsonToDocument(json.getJSONObject(key),stringBuffer))
+                        .append("</").append(key).append(">");
             }else{
-                stringBuffer.append("<").append(key).append(">").append(v).append("</").append(key).append(">");
+                if(Objects.nonNull(v)){
+                    stringBuffer
+                            .append("<").append(firstCharToUpCase(key)).append(">")
+                            .append("<![CDATA[").append(v).append("]]>")
+                            .append("</").append(firstCharToUpCase(key)).append(">");
+                }
             }
         });
         return stringBuffer.toString();
+    }
+
+    /**
+     * 字符串首字母转大写
+     * @param str
+     * @return
+     */
+    private static String firstCharToUpCase(String str){
+        return str.substring(0,1).toUpperCase()+str.substring(1);
+    }
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        TextMessageResponse response = new TextMessageResponse();
+        ReflectUtils.setDefaultValue(response);
+
+        final String json = JSON.toJSONString(response);
+        JSONObject jsonObject = JSON.parseObject(json);
+
+        System.out.println(jsonToXml(jsonObject));
     }
 }
