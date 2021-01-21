dc3b584e9be01e86f65eb5bd7e0d1d3a55a50f9a
冷春阳 <lengchunyang@qianlima.com>
2021-01-21 14:13:56 +0800

    fix

--- src/main/java/net/jlxxw/component/weixin/response/WeiXinMessageResponse.java
+++ src/main/java/net/jlxxw/component/weixin/response/WeiXinMessageResponse.java
@@ -1,5 +1,8 @@
 package net.jlxxw.component.weixin.response;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * 微信消息应答对象
  * @author chunyang.leng
@@ -28,9 +31,35 @@ public class WeiXinMessageResponse {
     private String MsgType;
 
     /**
-     * 通过素材管理中的接口上传多媒体文件，得到的id。
+     * 回复的消息内容（换行：在content中能够换行，微信客户端就支持换行显示）
      */
-    private String MediaId;
+    private String Content;
+
+    /**
+     * 图文消息个数；当用户发送文本、图片、语音、视频、图文、地理位置这六种消息时，开发者只能回复1条图文消息；其余场景最多可回复8条图文消息
+     */
+    private Integer ArticleCount;
+
+    /**
+     * 图文
+     */
+    private List<Article> Articles;
+    /**
+     * 图片
+     */
+    private Image image;
+    /**
+     * 音乐
+     */
+    private Music music;
+    /**
+     * 视频
+     */
+    private Video video;
+    /**
+     * 音频
+     */
+    private Voice voice;
 
     public String getToUserName() {
         return ToUserName;
@@ -64,6 +93,162 @@ public class WeiXinMessageResponse {
         MsgType = msgType;
     }
 
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
+
+    public Integer getArticleCount() {
+        return ArticleCount;
+    }
+
+    public void setArticleCount(Integer articleCount) {
+        ArticleCount = articleCount;
+    }
+
+    public List<Article> getArticles() {
+        return Articles;
+    }
+
+    public void setArticles(List<Article> articles) {
+        Articles = articles;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public Video getVideo() {
+        return video;
+    }
+
+    public void setVideo(Video video) {
+        this.video = video;
+    }
+
+    public Voice getVoice() {
+        return voice;
+    }
+
+    public void setVoice(Voice voice) {
+        this.voice = voice;
+    }
+
+    /**
+     * 回复文本消息
+     * @param content
+     * @return
+     */
+    public static WeiXinMessageResponse buildText(String content){
+        WeiXinMessageResponse weiXinMessageResponse = new WeiXinMessageResponse();
+        weiXinMessageResponse.setMsgType("text");
+        weiXinMessageResponse.setContent(content);
+        return weiXinMessageResponse;
+    }
+
+    /**
+     * 回复图片消息
+     * @param mediaId 通过素材管理中的接口上传多媒体文件，得到的id。
+     * @return
+     */
+    public static WeiXinMessageResponse buildImage(String mediaId){
+        WeiXinMessageResponse weiXinMessageResponse = new WeiXinMessageResponse();
+        weiXinMessageResponse.setMsgType("image");
+        Image image = new Image();
+        image.setMediaId(mediaId);
+        weiXinMessageResponse.setImage(image);
+        return weiXinMessageResponse;
+    }
+
+    /**
+     * 回复语音消息
+     * @param mediaId 通过素材管理中的接口上传多媒体文件，得到的id。
+     * @return
+     */
+    public static WeiXinMessageResponse buildVoice(String mediaId){
+        WeiXinMessageResponse weiXinMessageResponse = new WeiXinMessageResponse();
+        weiXinMessageResponse.setMsgType("voice");
+        Voice voice = new Voice();
+        voice.setMediaId(mediaId);
+        weiXinMessageResponse.setVoice(voice);
+        return weiXinMessageResponse;
+    }
+
+    /**
+     * 回复视频消息
+     * @param mediaId 通过素材管理中的接口上传多媒体文件，得到的id
+     * @param title 视频消息的标题
+     * @param description 视频消息的描述
+     * @return
+     */
+    public static WeiXinMessageResponse buildVideo(String mediaId,String title,String description){
+        WeiXinMessageResponse weiXinMessageResponse = new WeiXinMessageResponse();
+        weiXinMessageResponse.setMsgType("video");
+        Video video = new Video();
+        video.setMediaId(mediaId);
+        video.setTitle(title);
+        video.setDescription(description);
+        weiXinMessageResponse.setVideo(video);
+        return weiXinMessageResponse;
+    }
+
+    /**
+     * 回复音乐消息
+     * @param title 音乐标题
+     * @param description 音乐描述
+     * @param musicUrl 音乐链接
+     * @param hqMusicUrl 高质量音乐链接，WIFI环境优先使用该链接播放音乐
+     * @param thumbMediaId 缩略图的媒体id，通过素材管理中的接口上传多媒体文件，得到的id
+     * @return
+     */
+    public static WeiXinMessageResponse buildMusic(String title,String description,String musicUrl,String hqMusicUrl,String thumbMediaId){
+        WeiXinMessageResponse weiXinMessageResponse = new WeiXinMessageResponse();
+        weiXinMessageResponse.setMsgType("music");
+        Music music = new Music();
+        music.setTitle(title);
+        music.setDescription(description);
+        music.setMusicURL(musicUrl);
+        music.setHQMusicUrl(hqMusicUrl);
+        music.setThumbMediaId(thumbMediaId);
+        weiXinMessageResponse.setMusic(music);
+        return weiXinMessageResponse;
+    }
+
+    /**
+     * 回复图文消息
+     * @param articles
+     * @return
+     */
+    public static WeiXinMessageResponse buildArticle(Article... articles){
+        WeiXinMessageResponse weiXinMessageResponse = new WeiXinMessageResponse();
+        weiXinMessageResponse.setMsgType("news");
+        weiXinMessageResponse.setArticleCount(articles.length);
+        weiXinMessageResponse.setArticles(Arrays.asList(articles));
+        return weiXinMessageResponse;
+    }
+}
+
+class Image{
+    /**
+     * 通过素材管理中的接口上传多媒体文件，得到的id。
+     */
+    private String MediaId;
+
     public String getMediaId() {
         return MediaId;
     }
@@ -72,3 +257,179 @@ public class WeiXinMessageResponse {
         MediaId = mediaId;
     }
 }
+
+class Voice{
+    /**
+     * 通过素材管理中的接口上传多媒体文件，得到的id。
+     */
+    private String MediaId;
+
+    public String getMediaId() {
+        return MediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        MediaId = mediaId;
+    }
+}
+
+class Video{
+    /**
+     * 通过素材管理中的接口上传多媒体文件，得到的id。
+     */
+    private String MediaId;
+    /**
+     * 视频消息的标题
+     */
+    private String title;
+    /**
+     * 视频消息的描述
+     */
+    private String Description;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getMediaId() {
+        return MediaId;
+    }
+
+    public void setMediaId(String mediaId) {
+        MediaId = mediaId;
+    }
+}
+
+class Music{
+    /**
+     * 音乐标题
+     */
+    private String title;
+    /**
+     * 音乐描述
+     */
+    private String Description;
+
+    /**
+     * 音乐链接
+     */
+    private String MusicURL;
+
+    /**
+     * 高质量音乐链接，WIFI环境优先使用该链接播放音乐
+     */
+    private String HQMusicUrl;
+
+    /**
+     * 缩略图的媒体id，通过素材管理中的接口上传多媒体文件，得到的id
+     */
+    private String ThumbMediaId;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getMusicURL() {
+        return MusicURL;
+    }
+
+    public void setMusicURL(String musicURL) {
+        MusicURL = musicURL;
+    }
+
+    public String getHQMusicUrl() {
+        return HQMusicUrl;
+    }
+
+    public void setHQMusicUrl(String HQMusicUrl) {
+        this.HQMusicUrl = HQMusicUrl;
+    }
+
+    public String getThumbMediaId() {
+        return ThumbMediaId;
+    }
+
+    public void setThumbMediaId(String thumbMediaId) {
+        ThumbMediaId = thumbMediaId;
+    }
+}
+
+class Article{
+    /**
+     * 图文消息标题
+     */
+    private String Title;
+
+    /**
+     * 图文消息描述
+     */
+    private String Description;
+
+    /**
+     * 图片链接，支持JPG、PNG格式，较好的效果为大图360*200，小图200*200
+     *
+     */
+    private String PicUrl;
+
+    /**
+     * 点击图文消息跳转链接
+     */
+    private String Url;
+
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    public String getDescription() {
+        return Description;
+    }
+
+    public void setDescription(String description) {
+        Description = description;
+    }
+
+    public String getPicUrl() {
+        return PicUrl;
+    }
+
+    public void setPicUrl(String picUrl) {
+        PicUrl = picUrl;
+    }
+
+    public String getUrl() {
+        return Url;
+    }
+
+    public void setUrl(String url) {
+        Url = url;
+    }
+}
+
--- src/main/java/net/jlxxw/component/weixin/util/JsonToXmlUtils.java
+++ src/main/java/net/jlxxw/component/weixin/util/JsonToXmlUtils.java
@@ -1,7 +1,8 @@
 package net.jlxxw.component.weixin.util;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.util.CollectionUtils;
 import org.xml.sax.SAXException;
 
 import java.util.Objects;
@@ -42,11 +43,26 @@ public class JsonToXmlUtils {
                         .append("<").append(key).append(">")
                         .append(jsonToDocument(json.getJSONObject(key),stringBuffer))
                         .append("</").append(key).append(">");
-            }else{
+            }else if(child instanceof JSONArray) {
+                final JSONArray jsonArray = json.getJSONArray(key);
+                if(!CollectionUtils.isEmpty(jsonArray)){
+                    for (int i = 0; i < jsonArray.size(); i++) {
+                        Object obj = jsonArray.get(i);
+                        if(obj instanceof JSONObject){
+                            stringBuffer
+                                    .append("<").append(key).append(">")
+                                    .append(jsonToDocument(json.getJSONObject(key),stringBuffer))
+                                    .append("</").append(key).append(">");
+                        }else{
+                            // todo jsonArray and other
+                        }
+                    }
+                }
+            }else {
                 if(Objects.nonNull(v)){
                     stringBuffer
                             .append("<").append(firstCharToUpCase(key)).append(">")
-                            .append("<![CDATA[").append(v).append("]]>")
+                            .append(appendStr(v))
                             .append("</").append(firstCharToUpCase(key)).append(">");
                 }
             }
@@ -62,13 +78,19 @@ public class JsonToXmlUtils {
     private static String firstCharToUpCase(String str){
         return str.substring(0,1).toUpperCase()+str.substring(1);
     }
-    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
-        TextMessageResponse response = new TextMessageResponse();
-        ReflectUtils.setDefaultValue(response);
 
-        final String json = JSON.toJSONString(response);
-        JSONObject jsonObject = JSON.parseObject(json);
 
-        System.out.println(jsonToXml(jsonObject));
+    private static StringBuffer appendStr(Object obj){
+        StringBuffer stringBuffer  = new StringBuffer();
+        if(Objects.nonNull(obj)){
+            if(obj instanceof String){
+                stringBuffer
+                        .append("<![CDATA[").append(obj).append("]]>");
+            }else{
+                stringBuffer
+                        .append("<").append(obj).append(">");
+            }
+        }
+        return stringBuffer;
     }
 }
