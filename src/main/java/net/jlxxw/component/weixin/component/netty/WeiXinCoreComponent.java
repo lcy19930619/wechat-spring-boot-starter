3ecaee2a230529028d3216e7c6f67886f0265dc7
冷春阳 <lengchunyang@qianlima.com>
2021-01-25 10:14:29 +0800

    fix bootstrap

--- src/main/java/net/jlxxw/component/weixin/component/netty/WeiXinCoreComponent.java
+++ src/main/java/net/jlxxw/component/weixin/component/netty/WeiXinCoreComponent.java
@@ -2,10 +2,16 @@ package net.jlxxw.component.weixin.component.netty;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.stream.ChunkedWriteHandler;
 import net.jlxxw.component.weixin.properties.WeiXinNettyServerProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,7 +45,21 @@ public class WeiXinCoreComponent {
         bootstrap
                 .group(bossGroup, workGroup)
                 .channel(NioServerSocketChannel.class)
-                .childHandler(new WeiXinChannel())
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel socketChannel) throws Exception {
+                        // 请求解码器
+                        socketChannel.pipeline().addLast("http-decoder", new HttpRequestDecoder());
+                        // 将HTTP消息的多个部分合成一条完整的HTTP消息
+                        socketChannel.pipeline().addLast("http-aggregator", new HttpObjectAggregator(65535));
+                        // 响应转码器
+                        socketChannel.pipeline().addLast("http-encoder", new HttpResponseEncoder());
+                        // 解决大码流的问题，ChunkedWriteHandler：向客户端发送HTML5文件
+                        socketChannel.pipeline().addLast("http-chunked", new ChunkedWriteHandler());
+                        // 自定义处理handler
+                        socketChannel.pipeline().addLast("http-server", new WeiXinChannel());
+                    }
+                })
                 .localAddress(weiXinNettyServerProperties.getNettyPort())
                 //设置队列大小
                 .option(ChannelOption.SO_BACKLOG, weiXinNettyServerProperties.getQueueSize())
