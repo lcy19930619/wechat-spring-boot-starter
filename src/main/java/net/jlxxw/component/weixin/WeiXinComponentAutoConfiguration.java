b67cf3c74ea40775dd74d0a81948e533627bb829
冷春阳 <lengchunyang@qianlima.com>
2021-03-05 17:36:09 +0800

    fix 加入webflux

--- src/main/java/net/jlxxw/component/weixin/WeiXinComponentAutoConfiguration.java
+++ src/main/java/net/jlxxw/component/weixin/WeiXinComponentAutoConfiguration.java
@@ -1,5 +1,8 @@
 package net.jlxxw.component.weixin;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import net.jlxxw.component.weixin.function.token.WeiXinTokenManager;
 import net.jlxxw.component.weixin.mapper.TokenMapper;
 import net.jlxxw.component.weixin.properties.WeiXinProperties;
@@ -16,12 +19,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.ExchangeStrategies;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+import reactor.netty.tcp.TcpClient;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author chunyang.leng
@@ -99,9 +108,9 @@ public class WeiXinComponentAutoConfiguration {
         //获取到服务器的cpu内核
         int i = Runtime.getRuntime().availableProcessors();
         //核心池大小
-        executor.setCorePoolSize(i);
+        executor.setCorePoolSize(i * 2 - 1);
         //最大线程数
-        executor.setMaxPoolSize(i * 2);
+        executor.setMaxPoolSize(i * 4);
         //队列程度
         executor.setQueueCapacity(1000);
         //线程空闲时间
@@ -118,6 +127,7 @@ public class WeiXinComponentAutoConfiguration {
     @ConditionalOnProperty(prefix = "weixin", name = "enable-default-token-manager", havingValue = "true")
     public ScheduledUpdateToken weiXinTokenManager(TokenMapper tokenMapper,
                                                    WeiXinTokenManager weiXinTokenManager) {
+        logger.info("初始化默认token管理器");
         return new ScheduledUpdateToken(tokenMapper, weiXinTokenManager);
     }
 
@@ -128,6 +138,7 @@ public class WeiXinComponentAutoConfiguration {
             RestTemplate restTemplate,
             WeiXinServerSecurityCheck weiXinServerSecurityCheck,
             WeiXinProperties weiXinProperties) {
+        logger.info("初始化微信安全检查组件");
         return new ScheduledUpdateWeiXinServerIp(weiXinTokenManager, restTemplate, weiXinServerSecurityCheck, weiXinProperties);
     }
 
@@ -136,5 +147,28 @@ public class WeiXinComponentAutoConfiguration {
     public WeiXinServerSecurityCheck weiXinServerSecurityCheck() {
         logger.info("启用微信回调ip白名单管理器");
         return new WeiXinServerSecurityCheck();
+    }
+
+
+
+    @Bean
+    @ConditionalOnMissingBean(WebClient.class)
+    public WebClient webClient(){
+        logger.info("初始化WebClient");
+        TcpClient tcpClient = TcpClient
+                .create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .doOnConnected(connection -> {
+                    connection.addHandlerLast(new ReadTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                    connection.addHandlerLast(new WriteTimeoutHandler(5000, TimeUnit.MILLISECONDS));
+                });
+        return WebClient.builder()
+                .clientConnector(new ReactorClientHttpConnector(HttpClient.from(tcpClient)))
+                .exchangeStrategies(ExchangeStrategies.builder()
+                        .codecs(configurer -> configurer.defaultCodecs()
+                                .maxInMemorySize(16 * 1024 * 1024)
+                        )
+                        .build())
+                .build();
     }
 }
