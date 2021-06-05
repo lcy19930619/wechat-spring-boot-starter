ff99a9515615acd1b8129c8a79712dbfd5e5dda9
战秀梅 <1127537753@qq.com>
2021-06-05 21:08:12 +0800

    测试  提交代码验证项目是否加进来了

---
+++ src/test/java/net/jlxxw/component/weixin/function/push/Test.java
@@ -0,0 +1,10 @@
+package net.jlxxw.component.weixin.function.push;
+
+/**
+ * @author zhanxiumei
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
