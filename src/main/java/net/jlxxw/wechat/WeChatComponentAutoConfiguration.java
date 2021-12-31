4d7686a7db0c175ee8a386214af486d4701a83ca
冷春阳 <lcy19930619@163.com>
2021-12-31 10:47:00 +0800

    add 并发连接

--- src/main/java/net/jlxxw/wechat/WeChatComponentAutoConfiguration.java
+++ src/main/java/net/jlxxw/wechat/WeChatComponentAutoConfiguration.java
@@ -10,7 +10,15 @@ import net.jlxxw.wechat.schedul.ScheduledUpdateToken;
 import net.jlxxw.wechat.schedul.ScheduledUpdateWeChatServerIp;
 import net.jlxxw.wechat.security.WeChatServerSecurityCheck;
 import net.jlxxw.wechat.util.LoggerUtils;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.config.Registry;
+import org.apache.http.config.RegistryBuilder;
+import org.apache.http.conn.socket.ConnectionSocketFactory;
+import org.apache.http.conn.socket.PlainConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -45,9 +53,33 @@ public class WeChatComponentAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean(RestTemplate.class)
-    public RestTemplate restTemplate() {
-        return new RestTemplate(new HttpComponentsClientHttpRequestFactory(
-                HttpClientBuilder.create().build()));
+    public RestTemplate restTemplate(){
+        int defaultMaxPerRoute = 200;
+        Registry<ConnectionSocketFactory> registry = RegistryBuilder.<ConnectionSocketFactory>create()
+                .register("http", PlainConnectionSocketFactory.getSocketFactory())
+                .register("https", SSLConnectionSocketFactory.getSocketFactory())
+                .build();
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(registry);
+        //设置整个连接池最大连接数
+        connectionManager.setMaxTotal(2 * defaultMaxPerRoute);
+        //路由是对maxTotal的细分
+        connectionManager.setDefaultMaxPerRoute(defaultMaxPerRoute);
+        RequestConfig requestConfig = RequestConfig.custom()
+                //服务器返回数据(response)的时间，超过该时间抛出read timeout
+                .setSocketTimeout(10000)
+                //连接上服务器(握手成功)的时间，超出该时间抛出connect timeout
+                .setConnectTimeout(5000)
+                //从连接池中获取连接的超时时间，超过该时间未拿到可用连接，会抛出org.apache.http.conn.ConnectionPoolTimeoutException: Timeout waiting for connection from pool
+                .setConnectionRequestTimeout(1000)
+                .build();
+        CloseableHttpClient build = HttpClientBuilder.create()
+                .setDefaultRequestConfig(requestConfig)
+                // 如果不配置connectionManager，则默认使用System.getProperty获取配置参数
+                .setConnectionManager(connectionManager)
+                .build();
+        RestTemplate restTemplate = new RestTemplate(new HttpComponentsClientHttpRequestFactory(build));
+        LoggerUtils.info(logger,"初始化 RestTemplate");
+        return restTemplate;
     }
 
 
