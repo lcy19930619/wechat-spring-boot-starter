b8734f5f23cc06d04d6456b287d9dc27bf21d271
冷春阳 <lcy19930619@163.com>
2022-08-25 18:06:25 +0800

    add 加密模式消息编解码

--- src/test/java/net/jlxxw/wechat/listener/ListenerTest.java
+++ src/test/java/net/jlxxw/wechat/listener/ListenerTest.java
@@ -1,6 +1,17 @@
 package net.jlxxw.wechat.listener;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.annotation.PostConstruct;
 import net.jlxxw.wechat.base.BaseTest;
+import net.jlxxw.wechat.exception.AesException;
+import net.jlxxw.wechat.function.token.WeChatTokenManager;
+import net.jlxxw.wechat.properties.WeChatProperties;
+import net.jlxxw.wechat.util.SHA1;
+import net.jlxxw.wechat.util.WechatMessageCrypt;
 import org.apache.commons.io.IOUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,12 +27,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
 /**
  * @author chunyang.leng
  * @date 2021-12-18 7:00 下午
@@ -32,6 +37,20 @@ public class ListenerTest extends BaseTest {
     private RestTemplate restTemplate;
     @Value("${we-chat.netty.server.netty-port}")
     private int nettyPort;
+
+    @Autowired
+    private WeChatTokenManager weChatTokenManager;
+
+    @Autowired
+    private WeChatProperties weChatProperties;
+
+    private WechatMessageCrypt wechatMessageCrypt = null;
+    @PostConstruct
+    private void init() {
+        if (weChatProperties.isEnableMessageEnc()){
+            wechatMessageCrypt = new WechatMessageCrypt(weChatProperties.getEncodingAesKey(), weChatProperties.getAppId());
+        }
+    }
 
     @Test
     public void allListenerTest() throws IOException {
@@ -49,12 +68,23 @@ public class ListenerTest extends BaseTest {
             try (InputStream inputStream = resource.getInputStream();) {
                 List<String> list = IOUtils.readLines(inputStream, "utf-8");
                 String xmlData = String.join("\n", list);
+                String encParameters = "";
+                if (weChatProperties.isEnableMessageEnc()){
+                    String randomStr = wechatMessageCrypt.getRandomStr();
+                    String encrypt = wechatMessageCrypt.encrypt(randomStr, xmlData);
+                    long timeMillis = System.currentTimeMillis();
+                    String signature = SHA1.getSHA1(weChatProperties.getVerifyToken(), String.valueOf(timeMillis) , randomStr,encrypt);
+                    encParameters = "nonce="+randomStr + "&timestamp="+timeMillis + "&msg_signature=" +signature;
+                    xmlData = "<xml><Encrypt>" + encrypt + "</Encrypt><ToUserName>" + openId +"</ToUserName></xml>";
+                }
                 HttpHeaders headers = new HttpHeaders();
                 headers.setContentType(MediaType.APPLICATION_XML);
 
                 HttpEntity<String> formEntity = new HttpEntity<>(xmlData.toString(), headers);
-                ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://127.0.0.1:" + nettyPort, formEntity, String.class);
+                ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://127.0.0.1:" + nettyPort + "?" +encParameters, formEntity, String.class);
                 Assert.assertEquals(responseEntity.getStatusCode().value(), 200);
+            } catch (AesException e) {
+                throw new RuntimeException(e);
             }
         }
     }
