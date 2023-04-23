225a10cc6c7b6822b3c48ffdcb82a91b13df3d41
冷春阳 <lcy19930619@163.com>
2023-04-23 19:21:31 +0800

    微信团队会在近期移除推送中的部分内容，比如 first.DATA,remark.DATA等，包括自定义颜色

--- src/main/java/net/jlxxw/wechat/dto/template/WeChatTemplateDTO.java
+++ src/main/java/net/jlxxw/wechat/dto/template/WeChatTemplateDTO.java
@@ -1,15 +1,13 @@
 package net.jlxxw.wechat.dto.template;
 
-
 import com.alibaba.fastjson.annotation.JSONField;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import javax.validation.constraints.NotBlank;
-import net.jlxxw.wechat.aop.check.group.Insert;
-import net.jlxxw.wechat.enums.Color;
-
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import javax.validation.constraints.NotBlank;
+import net.jlxxw.wechat.aop.check.group.Insert;
+import net.jlxxw.wechat.enums.Color;
 
 /**
  * @author chunyang.leng
@@ -19,12 +17,12 @@ public class WeChatTemplateDTO {
     /**
      * 微信模版id
      */
-    @NotBlank(groups = Insert.class,message = "模版id不能为空")
+    @NotBlank(groups = Insert.class, message = "模版id不能为空")
     @JSONField(name = "template_id")
     @JsonProperty("template_id")
     private String templateId;
 
-    @NotBlank(groups = Insert.class,message = "用户openId不能为空")
+    @NotBlank(groups = Insert.class, message = "用户openId不能为空")
     private String touser;
     private String url;
     private Map<String, TemplateData> data;
@@ -100,7 +98,9 @@ public class WeChatTemplateDTO {
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除first 内容）
      */
+    @Deprecated
     public WeChatTemplateDTO buildFirstData(String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
@@ -115,7 +115,9 @@ public class WeChatTemplateDTO {
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除自定义颜色功能）
      */
+    @Deprecated
     public WeChatTemplateDTO buildKeyWord1Data(String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
@@ -125,12 +127,28 @@ public class WeChatTemplateDTO {
     }
 
     /**
+     * 构建 keyword1 节点的数据
+     *
+     * @param value 数据值
+     * @return 当前操作的模版对象
+     */
+    public WeChatTemplateDTO buildKeyWord1Data(String value) {
+        if (Objects.isNull(this.getData())) {
+            this.setData(new HashMap<>(16));
+        }
+        this.getData().put("keyword1", TemplateData.build(value, null));
+        return this;
+    }
+
+    /**
      * 构建 keyword2 节点的数据
      *
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除自定义颜色功能）
      */
+    @Deprecated
     public WeChatTemplateDTO buildKeyWord2Data(String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
@@ -140,12 +158,28 @@ public class WeChatTemplateDTO {
     }
 
     /**
+     * 构建 keyword2 节点的数据
+     *
+     * @param value 数据值
+     * @return 当前操作的模版对象
+     */
+    public WeChatTemplateDTO buildKeyWord2Data(String value) {
+        if (Objects.isNull(this.getData())) {
+            this.setData(new HashMap<>(16));
+        }
+        this.getData().put("keyword2", TemplateData.build(value, null));
+        return this;
+    }
+
+    /**
      * 构建 keyword3 节点的数据
      *
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除自定义颜色功能）
      */
+    @Deprecated
     public WeChatTemplateDTO buildKeyWord3Data(String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
@@ -155,12 +189,28 @@ public class WeChatTemplateDTO {
     }
 
     /**
+     * 构建 keyword3 节点的数据
+     *
+     * @param value 数据值
+     * @return 当前操作的模版对象
+     */
+    public WeChatTemplateDTO buildKeyWord3Data(String value) {
+        if (Objects.isNull(this.getData())) {
+            this.setData(new HashMap<>(16));
+        }
+        this.getData().put("keyword3", TemplateData.build(value, null));
+        return this;
+    }
+
+    /**
      * 构建 keyword4 节点的数据
      *
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除自定义颜色功能）
      */
+    @Deprecated
     public WeChatTemplateDTO buildKeyWord4Data(String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
@@ -170,12 +220,28 @@ public class WeChatTemplateDTO {
     }
 
     /**
+     * 构建 keyword4 节点的数据
+     *
+     * @param value 数据值
+     * @return 当前操作的模版对象
+     */
+    public WeChatTemplateDTO buildKeyWord4Data(String value) {
+        if (Objects.isNull(this.getData())) {
+            this.setData(new HashMap<>(16));
+        }
+        this.getData().put("keyword4", TemplateData.build(value, null));
+        return this;
+    }
+
+    /**
      * 构建 keyword5 节点的数据
      *
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除自定义颜色功能）
      */
+    @Deprecated
     public WeChatTemplateDTO buildKeyWord5Data(String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
@@ -185,12 +251,28 @@ public class WeChatTemplateDTO {
     }
 
     /**
+     * 构建 keyword5 节点的数据
+     *
+     * @param value 数据值
+     * @return 当前操作的模版对象
+     */
+    public WeChatTemplateDTO buildKeyWord5Data(String value) {
+        if (Objects.isNull(this.getData())) {
+            this.setData(new HashMap<>(16));
+        }
+        this.getData().put("keyword5", TemplateData.build(value, null));
+        return this;
+    }
+
+    /**
      * 构建 remark 节点的数据
      *
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除remark 内容）
      */
+    @Deprecated
     public WeChatTemplateDTO buildRemarkData(String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
@@ -205,12 +287,28 @@ public class WeChatTemplateDTO {
      * @param value 数据值
      * @param color 颜色信息
      * @return 当前操作的模版对象
+     * @deprecated 微信会在近期（2023.05.04 移除自定义颜色功能）
      */
+    @Deprecated
     public WeChatTemplateDTO buildOtherData(String key, String value, Color color) {
         if (Objects.isNull(this.getData())) {
             this.setData(new HashMap<>(16));
         }
         this.getData().put(key, TemplateData.build(value, color));
+        return this;
+    }
+
+    /**
+     * 构建 自定义 节点的数据
+     *
+     * @param value 数据值
+     * @return 当前操作的模版对象
+     */
+    public WeChatTemplateDTO buildOtherData(String key, String value) {
+        if (Objects.isNull(this.getData())) {
+            this.setData(new HashMap<>(16));
+        }
+        this.getData().put(key, TemplateData.build(value, null));
         return this;
     }
 }
