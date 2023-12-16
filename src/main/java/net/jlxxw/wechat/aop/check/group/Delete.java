353664e26cf5631a0677cc9839b45e5f5a1e68c2
deepin <lcy19930619@163.com>
2023-12-17 00:05:38 +0800

    临时保存

--- src/main/java/net/jlxxw/wechat/aop/check/group/Delete.java
+++
@@ -1,9 +0,0 @@
-package net.jlxxw.wechat.aop.check.group;
-
-/**
- * delete 系列检查
- * @author chunyang.leng
- * @date 2022-04-15 2:01 PM
- */
-public interface Delete {
-}
--- src/main/java/net/jlxxw/wechat/aop/check/group/Select.java
+++
@@ -1,9 +0,0 @@
-package net.jlxxw.wechat.aop.check.group;
-
-/**
- * select 系列检查
- * @author chunyang.leng
- * @date 2022-04-15 2:06 PM
- */
-public interface Select {
-}
