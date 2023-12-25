155ca5db8816eddbf8121aea9de5fc9e29dc02e1
冷春阳 <lcy19930619@163.com>
2023-12-25 17:43:00 +0800

    模块精简

--- starter/wechat-repository-mysql-spring-boot-starter/src/main/java/net/jlxxw/wechat/repository/mysql/mapper/TokenMapper.java
+++ starter/wechat-repository-mysql-spring-boot-starter/src/main/java/net/jlxxw/wechat/repository/mysql/mapper/TokenMapper.java
@@ -4,9 +4,6 @@ import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-import org.springframework.stereotype.Repository;
-
-import java.util.Date;
 
 /**
  * @author chunyang.leng
