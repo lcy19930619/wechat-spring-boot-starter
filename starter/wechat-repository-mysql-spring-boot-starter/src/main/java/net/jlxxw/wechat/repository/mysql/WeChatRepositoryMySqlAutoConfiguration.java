a5d01a9cfc8f38385850a59b7bfc57ea8f55bf4f
冷春阳 <lcy19930619@163.com>
2024-01-05 17:21:09 +0800

    fix

--- starter/wechat-repository-mysql-spring-boot-starter/src/main/java/net/jlxxw/wechat/repository/mysql/WeChatRepositoryMySqlAutoConfiguration.java
+++ starter/wechat-repository-mysql-spring-boot-starter/src/main/java/net/jlxxw/wechat/repository/mysql/WeChatRepositoryMySqlAutoConfiguration.java
@@ -2,15 +2,18 @@ package net.jlxxw.wechat.repository.mysql;
 
 import net.jlxxw.wechat.repository.aibot.WeChatAiBotTokenRepository;
 import net.jlxxw.wechat.repository.jsapi.WeChatJsApiTicketRepository;
+import net.jlxxw.wechat.repository.mysql.mapper.TokenMapper;
 import net.jlxxw.wechat.repository.mysql.properties.WeChatMySqlProperties;
+import net.jlxxw.wechat.repository.mysql.token.WeChatJsApiTicketRepositoryImpl;
+import net.jlxxw.wechat.repository.mysql.token.WeChatTokenRepositoryImpl;
 import net.jlxxw.wechat.repository.token.WeChatTokenRepository;
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
@@ -18,8 +21,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.simple.JdbcClient;
 
 import javax.sql.DataSource;
 
@@ -31,18 +32,17 @@ import javax.sql.DataSource;
  */
 @Configuration
 @AutoConfiguration(after = DataSourceAutoConfiguration.class)
-@ConditionalOnClass({ DataSource.class, JdbcTemplate.class })
 @ConditionalOnSingleCandidate(DataSource.class)
 @ComponentScan("net.jlxxw.wechat.repository.mysql")
+@MapperScan("net.jlxxw.wechat.repository.mysql.mapper")
 @ConditionalOnProperty(value = "wechat.repository.mysql.enable",havingValue = "true")
 public class WeChatRepositoryMySqlAutoConfiguration implements ApplicationRunner {
     private static final Logger logger = LoggerFactory.getLogger(WeChatRepositoryMySqlAutoConfiguration.class);
     @Autowired
     private WeChatMySqlProperties weChatMysqlProperties;
-    //@Autowired
-    //private TokenMapper tokenMapper;
     @Autowired
-    private JdbcClient jdbcClient;
+    private TokenMapper tokenMapper;
+
     /**
      * Callback used to run the bean.
      *
@@ -52,31 +52,12 @@ public class WeChatRepositoryMySqlAutoConfiguration implements ApplicationRunner
     @Override
     public void run(ApplicationArguments args) throws Exception {
         if (weChatMysqlProperties.isEnableAutoCreateJsApiTable()) {
-
-            // 自动创建 js api 表
-            jdbcClient.sql("create table if not exists wechat_js_api_ticket\n" +
-                    "(\n" +
-                    "    id          bigint(20) auto_increment primary key comment '数据自增主键',\n" +
-                    "    ticket       varchar(255) not null comment '从微信获取的ticket',\n" +
-                    "    create_time      datetime default now() comment '数据插入时间'\n" +
-                    ")\n" +
-                    "charset = utf8\n")
-                    .update();
-
-           // tokenMapper.createJsApiTicketTable();
+           tokenMapper.createJsApiTicketTable();
             logger.info( "已自动创建 token 表");
         }
 
         if (weChatMysqlProperties.isEnableAutoCreateTokenTable()) {
-            jdbcClient.sql("create table if not exists wechat_token\n" +
-                            "(\n" +
-                            "    id          bigint(20) auto_increment primary key comment '数据自增主键',\n" +
-                            "    token       varchar(255) not null comment '从微信获取的token',\n" +
-                            "    create_time      datetime default now() comment '数据插入时间'\n" +
-                            ")\n" +
-                            "charset = utf8\n")
-                    .update();
-           // tokenMapper.createTokenTable();
+           tokenMapper.createTokenTable();
             logger.info( "已自动创建 js_api 表");
         }
 
@@ -84,65 +65,13 @@ public class WeChatRepositoryMySqlAutoConfiguration implements ApplicationRunner
 
     @Bean
     public WeChatTokenRepository weChatTokenRepository() {
-        return new WeChatTokenRepository(){
-            /**
-             * 保存token
-             *
-             * @param token
-             */
-            @Override
-            public void save(String token) {
-                jdbcClient
-                        .sql("insert into wechat_token (token) values (:token)")
-                        .param("token",token)
-                        .update();
-            }
-
-            /**
-             * 获取保存在本地的token
-             *
-             * @return 保存在本地的token
-             */
-            @Override
-            public String get() {
-                JdbcClient.MappedQuerySpec<String> query = jdbcClient
-                        .sql("select token from wechat_token order by id desc limit 1")
-                        .query(String.class);
-                return query.single();
-            }
-        };
+        return new WeChatTokenRepositoryImpl(tokenMapper);
     }
 
 
     @Bean
     public WeChatJsApiTicketRepository weChatJsApiTicketRepository() {
-        return new WeChatJsApiTicketRepository(){
-            /**
-             * 存储数据
-             *
-             * @param jsApiTicket
-             */
-            @Override
-            public void save(String jsApiTicket) {
-                jdbcClient
-                        .sql("insert into wechat_js_api_ticket (ticket) values (:ticket)")
-                        .param("ticket",jsApiTicket)
-                        .update();
-            }
-
-            /**
-             * 读取数据
-             *
-             * @return
-             */
-            @Override
-            public String get() {
-                JdbcClient.MappedQuerySpec<String> query = jdbcClient
-                        .sql("select ticket from wechat_js_api_ticket order by id desc limit 1")
-                        .query(String.class);
-                return query.single();
-            }
-        };
+        return new WeChatJsApiTicketRepositoryImpl(tokenMapper);
     }
 
     @Bean
--- starter/wechat-repository-mysql-spring-boot-starter/src/test/java/net/jlxxw/wechat/repository/mysql/CreateTableTest.java
+++ starter/wechat-repository-mysql-spring-boot-starter/src/test/java/net/jlxxw/wechat/repository/mysql/CreateTableTest.java
@@ -1,9 +1,7 @@
 package net.jlxxw.wechat.repository.mysql;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.data.jdbc.DataJdbcTest;
-import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
@@ -13,12 +11,9 @@ import org.springframework.test.context.TestPropertySource;
  */
 @TestPropertySource("classpath:application-*.yml")
 @ActiveProfiles("table")
-@DataJdbcTest
-//@SpringBootTest(classes=WeChatRepositoryMySqlAutoConfiguration.class)
+@SpringBootTest(classes=WeChatRepositoryMySqlAutoConfiguration.class)
 public class CreateTableTest {
 
-    @Autowired
-    private JdbcClient jdbcClient;
     @Test
     public void testTokenTableExists(){
 
