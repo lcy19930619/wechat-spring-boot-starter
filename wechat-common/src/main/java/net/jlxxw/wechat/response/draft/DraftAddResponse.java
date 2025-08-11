f811ebf2ebb066eeb72cc0a08623146775ba2724
冷春阳 <lcy19930619@163.com>
2025-08-11 15:46:41 +0800

    调整添加草稿逻辑

--- wechat-common/src/main/java/net/jlxxw/wechat/response/draft/DraftAddResponse.java
+++ wechat-common/src/main/java/net/jlxxw/wechat/response/draft/DraftAddResponse.java
@@ -1,14 +1,20 @@
 package net.jlxxw.wechat.response.draft;
 
+import com.alibaba.fastjson.annotation.JSONField;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import net.jlxxw.wechat.response.WeChatResponse;
 
 /**
  * 新增草稿响应
+ * 
+ * @see <a href="https://developers.weixin.qq.com/doc/service/api/draftbox/draftmanage/api_draft_add.html">文档地址</a>
  */
 public class DraftAddResponse extends WeChatResponse {
     /**
      * 新增的图文消息素材id
      */
+    @JSONField(name = "media_id")
+    @JsonProperty(value = "media_id")
     private String mediaId;
     
 
